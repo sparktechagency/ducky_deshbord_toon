324cbd24d7b66b5a3ffc8b60b7b0b05a10b2f8ec
humayonitbd <humayonforid44@gmail.com>
2025-06-12 09:18:33 +0600

    update server js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react({fastRefresh: false})],
+  plugins: [react({ fastRefresh: false })],
   server: {
-    host: "0.0.0.0",
-    port: 3050,
+    host: '0.0.0.0',
+    port: 4173,
   },
 });
